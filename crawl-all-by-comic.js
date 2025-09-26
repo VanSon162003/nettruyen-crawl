febc41f03cdc9e3920a145cdf7e01a69add591c1
van son <vanson16032003@gmailcom>
2025-09-26 17:07:15 +0700

    updates

--- crawl-all-by-comic.js
+++ crawl-all-by-comic.js
@@ -1,92 +1,44 @@
-const puppeteer = require("puppeteer");
-
-const { Comic, Author, Genre, AuthorComic, GenreComic } = require("./models");
-const getRandomUserAgent = require("./utils/getRandomUserAgent");
-const { where } = require("sequelize");
-
-async function scrapeComic(item) {
-    let browser;
-    try {
-        browser = await puppeteer.launch({
-            headless: true,
-            args: ["--no-sandbox", "--disable-setuid-sandbox"],
-        });
-
-        const page = await browser.newPage();
-        await page.setUserAgent(getRandomUserAgent());
-        await page.goto(item.originalUrl, { waitUntil: "networkidle0" });
-
-        const data = await page.evaluate(() => {
-            const author = document.querySelector(
-                ".author > .name + p"
-            ).innerText;
-
-            const genres =
-                document.querySelector(".kind > .name + p").innerText;
-
-            const views =
-                document.querySelector(
-                    ".list-info li:nth-child(4) p:last-child"
-                )?.innerText || 0;
-
-            const ratings = document.querySelector(
-                'span[itemprop="ratingValue"]'
-            ).innerText;
-
-            return { author, genres, views, ratings };
-        });
-
-        await browser.close();
-        return data;
-    } catch (error) {
-        if (browser) {
-            await browser.close();
-        }
-        console.error(error);
-        return null;
-    }
-}
-
 async function start() {
     const comics = await Comic.findAll();
 
     for (const item of comics) {
         const data = await scrapeComic(item);
+        if (!data) {
+            console.log(`Skip comic ${item.id}, scrape failed`);
+            continue;
+        }
 
-        const genres = data?.genres?.split(" - ");
+        const genres = data.genres ? data.genres.split(" - ") : [];
 
-        item.views = data.views;
-        item.ratings = data.ratings;
-
+        item.views = data.views || 0;
+        item.ratings = data.ratings || null;
         await item.save();
 
         for (const genre of genres) {
             const trimmed = genre?.trim();
             if (!trimmed) continue;
 
-            const [gen, create] = await Genre.findOrCreate({
-                where: {
-                    name: trimmed,
-                },
+            const [gen] = await Genre.findOrCreate({
+                where: { name: trimmed },
+                defaults: { name: trimmed },
             });
 
-            GenreComic.create({
-                genreId: gen.id,
-                comicId: item.id,
+            await GenreComic.findOrCreate({
+                where: { genreId: gen.id, comicId: item.id },
+                defaults: { genreId: gen.id, comicId: item.id },
             });
         }
 
-        const [author, create] = await Author.findOrCreate({
-            where: {
-                name: data.author,
-            },
-        });
+        if (data.author) {
+            const [author] = await Author.findOrCreate({
+                where: { name: data.author },
+                defaults: { name: data.author },
+            });
 
-        AuthorComic.create({
-            author_id: author.id,
-            comic_id: item.id,
-        });
+            await AuthorComic.findOrCreate({
+                where: { author_id: author.id, comic_id: item.id },
+                defaults: { author_id: author.id, comic_id: item.id },
+            });
+        }
     }
 }
-
-start();
